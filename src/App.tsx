dd9643a45bb34e1ce246a7189478a1e31886c5e1
unknown <mario.krastev@mentormate.com>
2022-06-02 15:47:54 +0300

    Add libraries

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Button from "@mui/material/Button";
+const App = () => {
+  return (
+    <div className="App">
+      <Button variant="contained">Click</Button>
+    </div>
+  );
+};
+
+export default App;
